38a6e7e12288ac39564de4c00d007373c80510b2
murakami <tmaqua007@gmail.com>
2016-05-28 03:13:45 +0900

    gitlab

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -2,22 +2,23 @@
 $worker  = 2
 $timeout = 30
 
-$shared_path = "/var/www/rails/Capistrano/shared/"
-$current_path = "/var/www/rails/Capistrano/current"
+# $shared_path = "/var/www/rails/Capistrano/shared/"
+# $current_path = "/var/www/rails/Capistrano/current"
 
-$listen = File.expand_path('tmp/sockets/unicorn.sock', $shared_path)
-$pid = File.expand_path('tmp/pids/unicorn.pid', $shared_path)
-$std_log = File.expand_path('log/unicorn.log', $shared_path)
+# $listen = File.expand_path('tmp/sockets/unicorn.sock', $shared_path)
+# $pid = File.expand_path('tmp/pids/unicorn.pid', $shared_path)
+# $std_log = File.expand_path('log/unicorn.log', $shared_path)
 
-# $app_path = "/var/www/rails/Capistrano/current"
-# $app_dir = "/var/www/rails/Capistrano"
-# $listen  = File.expand_path 'shared/tmp/sockets/.unicorn.sock', $app_dir
-# $pid     = File.expand_path 'shared/tmp/pids/unicorn.pid', $app_dir
-# $std_log = File.expand_path 'shared/log/unicorn.log', $app_dir
+$app_path = "/var/www/rails/Capistrano/current"
+$app_dir = "/var/www/rails/Capistrano"
+$listen  = File.expand_path 'shared/tmp/sockets/.unicorn.sock', $app_dir
+$pid     = File.expand_path 'shared/tmp/pids/unicorn.pid', $app_dir
+$std_log = File.expand_path 'shared/log/unicorn.log', $app_dir
 
 # set config
 worker_processes  $worker
-working_directory $current_path
+# working_directory $current_path
+working_directory $app_path
 stderr_path $std_log
 stdout_path $std_log
 timeout $timeout

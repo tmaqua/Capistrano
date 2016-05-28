1eb573a0911f66e880e673b49cbe99f7893196e8
murakami <tmaqua007@gmail.com>
2016-05-28 15:53:31 +0900

    hoge

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -35,7 +35,7 @@ working_directory "#{app_path}/current"
 # we use a shorter backlog for quicker failover when busy
 #listen "/path/to/.unicorn.sock", :backlog => 64
 #listen 8080, :tcp_nopush => true
-listen "#{app_path}/shared/tmp/sockets/unicorn.sock", :backlog => 64
+listen "#{app_path}/shared/tmp/sockets/.unicorn.sock", :backlog => 64
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 #timeout 30

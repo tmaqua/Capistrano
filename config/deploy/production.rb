75d733e8afeb4c874b77c6cb2b60b75521a78b57
murakami <tmaqua007@gmail.com>
2016-06-01 03:01:01 +0900

    deploy elb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,14 +7,37 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server '52.68.208.114', user: 'fankami', roles: %w{app db web}
+require 'aws-sdk'
+
+elb = Aws::ElasticLoadBalancing::Client.new(
+  region: ENV["AWS_REGION"],
+  access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+  secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+)
+
+instance_states = 
+  elb.describe_instance_health(load_balancer_name: ENV["AWS_ELB_NAME"]).instance_states
+
+target_instance_ids = []
+instance_states.each do |instance_state|
+  if instance_state.state == 'InService'
+    target_instance_ids.push(instance_state.instance_id) 
+  end
+end
+
+ec2 = Aws::EC2::Client.new
+response = ec2.describe_instances instance_ids: target_instance_ids
+ip_addresses = response.reservations.map {|r| r.instances.map {|i| i.public_ip_address } }.flatten
+
+ip_addresses.each do |ip|
+  server ip.to_s, user: 'fankami', roles: %w{app db web}
+end
+# server '52.68.208.114', user: 'fankami', roles: %w{app db web}
 set :ssh_options, {
     keys: %w(~/.ssh/aws.fankami/aws_fankami),
     forward_agent: true,
     auth_methods: %w(publickey)
   }
-
-
 
 # role-based syntax
 # ==================

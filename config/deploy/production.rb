aef2cf565d68cb0d7dfaf71df098e4725e2500ef
murakami <tmaqua007@gmail.com>
2016-06-01 03:04:31 +0900

    deploy elb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,9 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
+require 'dotenv'
 require 'aws-sdk'
+Dotenv.load
 
 elb = Aws::ElasticLoadBalancing::Client.new(
   region: ENV["AWS_REGION"],

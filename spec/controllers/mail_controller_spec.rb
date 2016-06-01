e0927c1e04dda5542121e2699913e0208f2d28cc
murakami <tmaqua007@gmail.com>
2016-06-02 04:33:22 +0900

    fixed mail rspec

--- spec/controllers/mail_controller_spec.rb
+++ spec/controllers/mail_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe MailController, type: :controller do
 
-  describe "GET #send" do
+  describe "GET #send_mail" do
     it "returns http success" do
-      get :send
+      get :send_mail
       expect(response).to have_http_status(:success)
     end
   end

5cf67542c059ccbeb62583dc743361143314aedf
murakami <tmaqua007@gmail.com>
2016-06-03 03:36:20 +0900

    self

---
+++ config/deploy/local.rb
@@ -0,0 +1,9 @@
+server 'localhost', user: 'fankami', roles: %w(web app db)
+
+set :stage, 'local'
+set :branch, 'master'
+set :rails_env, :production
+
+set :ssh_options, {
+  keys: %w(~/.ssh/id_for_localhost),
+}
